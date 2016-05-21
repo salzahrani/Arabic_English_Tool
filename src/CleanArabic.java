58a631c7bd269b828acc41d32c7d253c31633c75
Sultan Alzahrani <ssalzahr@asu.edu>
2016-05-21 12:13:03 -0700

    First commit after recovery 5/21/2016

--- src/CleanArabic.java
+++ src/CleanArabic.java
@@ -6,42 +6,34 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class CleanArabic {
-    public static String folder1  = "C:/Users/Sultan/IdeaProjects/JavaArabic/corpora/original/FamilyWomenRisingKids/";
-    public static String folder2  = "C:/Users/Sultan/IdeaProjects/JavaArabic/corpora/original/ReligionFatwa/";
 
+    public static String main_folder = "C:/Users/Sultan/IdeaProjects/JavaArabic/";
+    public static String corpora_folder = main_folder + "corpora/";
+    public static String original_folder = corpora_folder + "original/";
+    public static String folder1_original = original_folder + "FamilyWomenRisingKids/";
+    public static String folder2_original = original_folder + "ReligionFatwa/";
+    public static String arff_file_original = "./weka/docs_original.arff";
+
+    public static String filter_folder = corpora_folder + "filtered/";
+    public static String folder1_filter  = filter_folder + "FamilyWomenRisingKids/";
+    public static String folder2_filter  = filter_folder + "ReligionFatwa/";
+    public static String arff_file_filter = "./weka/docs_filtered.arff";
     public static void main(String[] args)
     {
-        System.out.println("Second Hello World!");
-        //String file_name_to_read = "./weka/docs.arff";
-        //String file_name_to_read = "C:\\Users\\Sultan\\IdeaProjects\\JavaArabic\\weka\\docs.arff";
-        //String file_name_to_read = "C:/Users/Sultan/Desktop/tmp/test.txt";
-        String file_name_to_read = "C:/Users/Sultan/Desktop/tmp/tes2t.txt";
-        String file_name_to_write = "./weka/docs_2.arff";
 
-        //ReadWriteOneLine(file_name_to_read,file_name_to_write);
+        readCrateSameDirectoriesAsOrgn();
+        // To create ARFF there is a two configuration:
+        // (1) Create from the original folder
+        // (2) Create from from the filtered folder.
+        createARFF(folder1_original,folder2_original,arff_file_original);
+        createARFF(folder1_filter,folder2_filter,arff_file_filter);
+        copyingTextFilesFromFolderToAnother(folder1_original,folder1_filter);
+        copyingTextFilesFromFolderToAnother(folder2_original,folder2_filter);
 
-        ArrayList<String> a_lst_1 = readFolderToList(folder1);
-        ArrayList<String> a_lst_2 = readFolderToList(folder2);
-
-        String cls1 = "FamilyWomenRisingKids";
-        String cls2 = "ReligionFatwa";
-
-        print_arff("./weka/docs_manual.arff",cls1,cls2,a_lst_1,a_lst_2);
-
-
-        /*
-        String current = null;
-        try {
-            current = new File( "." ).getCanonicalPath();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Current dir:"+current);
-        */
     }
+
 
     /*public static void ReadWriteOneLine(String fileName_toread,String fileName_towrite)
     {
@@ -63,6 +55,71 @@ public class CleanArabic {
 
     }
     */
+
+    public static void initialize_folder(String filderName)
+    {
+        File file = new File(filderName);
+        if (!file.exists())
+        {
+            if (file.mkdir())
+            {
+                System.out.println("Directory is created!");
+            } else
+            {
+                System.out.println("Failed to create directory!");
+            }
+        }
+        else
+        {
+            File[] flist = file.listFiles();
+            if (flist != null && flist.length > 0) {
+                for (File f : flist) {
+                    f.delete();
+                }
+            }
+        }
+
+    }
+    public static void readCrateSameDirectoriesAsOrgn()
+    {
+        // It will try creating 2 folders in fiiltered folder as it listed under
+        // original, if already created, then it would basically wiped its content.
+        initialize_folder(folder1_filter);
+        initialize_folder(folder2_filter);
+
+        // first reading textual content in each folder and wirte it as it is to its
+        // corresponding folder.
+
+
+    }
+
+
+    public static void copyingTextFilesFromFolderToAnother(String originalFolder,
+                                                           String targetFolder)
+    {
+        File folder = new File(originalFolder);
+        File[] listOfFiles = folder.listFiles();
+        ArrayList<String> a_list = new ArrayList<String>();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            File file = listOfFiles[i];
+            if (file.isFile() && file.getName().endsWith(".txt")) {
+                try {
+                    String content = FileUtils.readFileToString(file,"UTF-8");
+                    String content_after_process = content;
+                    String fileName = file.getName();
+                    //System.out.println("File Name: "+ fileName);
+
+                    writeToFile(targetFolder+fileName,content_after_process);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+    /* do somthing with content */
+            }
+        }
+
+    }
 
     public static ArrayList<String> readFolderToList(String folderName)
     {
@@ -86,18 +143,67 @@ public class CleanArabic {
         return a_list;
     }
 
+    // This function create a single arff containing all textual contents of original folder....
+    public static void createARFF(String folder1, String folder2,String arff_file)
+    {
+        System.out.println("Creating ARFF file");
+
+        ArrayList<String> a_lst_1 = readFolderToList(folder1);
+        ArrayList<String> a_lst_2 = readFolderToList(folder2);
+
+        String cls1 = "FamilyWomenRisingKids";
+        String cls2 = "ReligionFatwa";
+
+        print_arff(arff_file,cls1,cls2,a_lst_1,a_lst_2);
+
+
+        /*
+        String current = null;
+        try {
+            current = new File( "." ).getCanonicalPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Current dir:"+current);
+        */
+    }
+
+    public static void writeToFile(String fileName, String str)
+    {
+        try
+        {
+            // create new file
+            File file = new File(fileName);
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            // write in file
+            bw.write(str);
+            bw.flush();
+            bw.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
     public static void print_arff(String fileName,String cls1,
                                   String cls2,ArrayList<String> a1,
                                   ArrayList<String> a2)
     {
         String header =
-        "@relation C__Users_Sultan_IdeaProjects_JavaArabic_corpora \n" +
+                "@relation C__Users_Sultan_IdeaProjects_JavaArabic_corpora \n" +
 
-        "@attribute text string \n" +
+                        "@attribute text string \n" +
 
-        "@attribute @@class@@ {FamilyWomenRisingKids,ReligionFatwa} \n" +
+                        "@attribute @@class@@ {FamilyWomenRisingKids,ReligionFatwa} \n" +
 
-        "@data \n";
+                        "@data \n";
         try{
             // create new file
             File file = new File(fileName);
