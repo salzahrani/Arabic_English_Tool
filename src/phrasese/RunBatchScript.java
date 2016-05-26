c42a1160a1bcd9209e1649b177a9eac90e2f7685
Sultan <salzahrani@gmail.com>
2016-05-26 11:47:00 -0700

    Integeration PhraseMinder (2)

--- src/phrasese/RunBatchScript.java
+++ src/phrasese/RunBatchScript.java
@@ -9,7 +9,7 @@ public class RunBatchScript {
     public static void main(String[] args)
     {
         System.out.println("Now I am trying to run an script");
-        String path="cmd /c start C:\\Users\\Sultan\\IdeaProjects\\JavaArabic\\PhraseMiner\\topicalPhrases\\win_run.bat";
+        String path="cmd /c start C:\\Users\\Sultan\\IdeaProvfjects\\JavaArabic\\PhraseMiner\\topicalPhrases\\win_run.bat";
         Runtime rn=Runtime.getRuntime();
         try {
             Process pr=rn.exec(path);
