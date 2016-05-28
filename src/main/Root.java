743938d7dbb5b7bfab1857fddcec34c93281736d
Sultan <salzahrani@gmail.com>
2016-05-28 14:28:38 -0700

    Adding Root package

---
+++ src/main/Root.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by Sultan on 5/28/2016.
+ */
+public class Root
+    {
+        public static void main(String args)
+        {
+            // Call file to generate files...
+            // Call LDA
+            // Call Phrase Minner
+        }
+}
